7fe2feba7fa9c46bf831d1b533d11c408e5ebd60
jdecruzdeusto <juande.cruz@opendeusto.es>
2024-05-25 11:23:40 +0200

    cambios finales

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Abre la primera terminal y ejecuta el comando de Docker para Grafana
-gnome-terminal -- bash -c "sudo docker run -d -p 3000:3000 grafana/grafana; exec bash"
-
-# Abre la segunda terminal y ejecuta el comando para iniciar el servidor con Uvicorn
-gnome-terminal -- bash -c "uvicorn api:app --reload; exec bash"
